f3771b6d93bad02bb5d846c2e03b6e800d3398bd
Syed <syedarshadmehdi@gmail.com>
2021-10-14 14:33:59 +1100

    created users and schedules get

--- index.js
+++ index.js
@@ -9,57 +9,52 @@ const PORT = process.env.PORT || 3000
 //JSON and form Parsing middleware
 
 app.use(express.json())
-app.use(express.urlencoded({ extended : true }))
+app.use(express.urlencoded({ extended: true }))
 
 //Routes
 app.get('/', (req, res) => {
-    res.send("<h1>Welcome to our schedule website</h1>")
+    res.send("<h1>Welcome to our Schedule Website</h1>")
 })
 
 //Get all users
-app.get('/users', (req,res) => {
-res.send(data.users)
+app.get('/users', (req, res) => {
+    res.send(data.users)
+})
+//Get all schedules
+app.get('/schedules', (req, res) => {
+    res.send(data.schedules)
 })
 
-//Get all Schedules
+//Get Individual User Id
+app.get('/users/:id', (req, res) => {
+    const user = data.users[req.params.id]
+    res.send(user)
+})
 
-app.get('/schedules', (req,res) => {
-    res.send(data.schedules)
-    })
-
-//Get all posts
-
-app.get('/posts', (req,res) => {
-    res.send(data.posts)
-    })
-
-
-    //Get Individual User
-
-    app.get('/users/:Id', (req,res) => {
-        const user = data.users[req.params.id]
-        res.send()
-        })
+//Get Individual Schedules
+app.get('/schedules/:id', (req, res) => {
+    const schedule = data.schedules[req.params.id]
+    res.send(schedule)
+})
 
 
+//Create new user
 
-       //Create new user
-
-       app.post('/posts',(req, res)=> {
-           //Add post to all posts
-        data.posts.push(req.body)
-        res.send(req.body)
-       })
+app.post('/posts', (req, res) => {
+    //Add post to all posts
+    data.posts.push(req.body)
+    res.send(req.body)
+})
 
 
-       //Create new user
+//Create new user
 
-       app.post('/users',(req, res)=> {
-           const password = req.body.password
-           const salt = bcrypt.genSaltSync(10)
-           const hash = bcrypt.hashSync(password, salt)
-           res.send(hash)
-       })
+app.post('/users', (req, res) => {
+    const password = req.body.password
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    res.send(hash)
+})
 //CRUD - Create, Read, Update, Delete
 
 //CRUD -   Post, get, put/patch, delete
@@ -68,3 +63,5 @@ app.get('/posts', (req,res) => {
 app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`)
 })
+
+
