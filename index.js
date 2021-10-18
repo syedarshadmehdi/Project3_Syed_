6809749c6e1a20f5efdfc6e30b6e0418182dcd71
Syed <syedarshadmehdi@gmail.com>
2021-10-18 16:48:06 +1100

    SUCCESSFULLY PUSH THE NEW USER

--- index.js
+++ index.js
@@ -32,6 +32,48 @@ app.get('/users', (req, res) => {
     })
 })
 
+//Get all posts
+app.post('./posts', (req, res) => {
+    res.send(data.posts)
+})
+
+//Create New post
+
+app.post('/posts', (req, res) => {
+    data.posts.push(req.body)
+    res.send(req.body)
+})
+
+//Add new user
+app.get('/users/add', (req, res) => {
+    res.render('pages/new-user')
+})
+
+//Get Individual user
+
+app.get('/users/:id', (req, res) => {
+    const user = data.users[req.params.id]
+    res.send(user)
+})
+
+
+//Create new user
+app.post('/users', (req, res) => {
+    //using bcryptjs
+    const password = req.body.password
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password,salt)
+
+    //Add hash to user object and then push to user Array
+    data.users.push({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: hash
+    })
+
+    res.redirect('/users')
+})
 app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`)
 })
