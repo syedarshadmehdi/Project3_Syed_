78dbd6e3d2c7fdfb3cbc2d5ec4a9f1d3537497c5
Syed <syedarshadmehdi@gmail.com>
2021-10-18 16:10:54 +1100

    try to show the data on browser

--- index.js
+++ index.js
@@ -1,64 +1,36 @@
 const express = require('express')
 const data = require('./data')
 const bcrypt = require('bcryptjs')
+const morgan = require('morgan')
 const app = express()
+
 
 const PORT = process.env.PORT || 3000
 
 
-//JSON and form Parsing middleware
-
+//body parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//Routes
+//logging middleware
+app.use(morgan('dev'))
+
+//Set view engine
+app.set('view engine', 'ejs')
+
+
+//Routing
+//Welcome page
 app.get('/', (req, res) => {
-    res.send("<h1>Welcome to our Schedule Website</h1>")
+    res.render('pages/home')
 })
 
 //Get all users
 app.get('/users', (req, res) => {
-    res.send(data.users)
+    res.render('pages/users', {
+        users: data.users
+    })
 })
-//Get all schedules
-app.get('/schedules', (req, res) => {
-    res.send(data.schedules)
-})
-
-//Get Individual User Id
-app.get('/users/:id', (req, res) => {
-    const user = data.users[req.params.id]
-    res.send(user)
-})
-
-//Get Individual Schedules
-app.get('/schedules/:id', (req, res) => {
-    const schedule = data.schedules[req.params.id]
-    res.send(schedule)
-})
-
-
-//Create new user
-
-app.post('/posts', (req, res) => {
-    //Add post to all posts
-    data.posts.push(req.body)
-    res.send(req.body)
-})
-
-
-//Create new user
-
-app.post('/users', (req, res) => {
-    const password = req.body.password
-    const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(password, salt)
-    res.send(hash)
-})
-//CRUD - Create, Read, Update, Delete
-
-//CRUD -   Post, get, put/patch, delete
-
 
 app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`)
