6e456896488d4b727a323ed7efcf08d567685009
Syed <syedarshadmehdi@gmail.com>
2021-10-21 12:02:53 +1100

    create 3c project and files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const data = require('./data')
 const bcrypt = require('bcryptjs')
+const db = require('./database')
 const app = express()
 
 const PORT = process.env.PORT || 3000
@@ -11,53 +12,26 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//set engine
+app.set('view engine', 'ejs')
+
 //Routes
 app.get('/', (req, res) => {
-    res.send("<h1>Welcome to our Schedule Website</h1>")
+    res.render('pages/home')
 })
 
-//Get all users
-app.get('/users', (req, res) => {
-    res.send(data.users)
-})
-//Get all schedules
-app.get('/schedules', (req, res) => {
-    res.send(data.schedules)
-})
-
-//Get Individual User Id
-app.get('/users/:id', (req, res) => {
-    const user = data.users[req.params.id]
-    res.send(user)
-})
-
-//Get Individual Schedules
-app.get('/schedules/:id', (req, res) => {
-    const schedule = data.schedules[req.params.id]
-    res.send(schedule)
+//Homepage
+app.get('/', (req, res) => {
+    res.send //send back raw data
+    res.json //send back raw data converts to json if needed
+    res.render // send back a compiled html pages eg: homepage, contactpage
+    res.redirect //redirect to a new url/route eg: www.google.com
+    res.end // end the request/ processing / send nothing back
 })
 
 
-//Create new user
-
-app.post('/posts', (req, res) => {
-    //Add post to all posts
-    data.posts.push(req.body)
-    res.send(req.body)
-})
 
 
-//Create new user
-
-app.post('/users', (req, res) => {
-    const password = req.body.password
-    const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(password, salt)
-    res.send(hash)
-})
-//CRUD - Create, Read, Update, Delete
-
-//CRUD -   Post, get, put/patch, delete
 
 
 app.listen(PORT, () => {
